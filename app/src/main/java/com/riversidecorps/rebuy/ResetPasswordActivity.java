3a95532a1ffde87161907e38e642b54cfa21fabe
Seannycow <sean.carmichael2@gmail.com>
2017-09-14 17:43:14 +1000

    Revert "Merging Yijun's work"

--- app/src/main/java/com/riversidecorps/rebuy/ResetPasswordActivity.java
+++ app/src/main/java/com/riversidecorps/rebuy/ResetPasswordActivity.java
@@ -23,7 +23,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.riversidecorps.rebuy.AccountManagement.LoginActivity;
 
 import static android.content.ContentValues.TAG;
 
