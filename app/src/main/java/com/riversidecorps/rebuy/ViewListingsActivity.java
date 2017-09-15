df9d789e6febbd427cc31617314dce06fa80f860
Yijun Gai <bevery123@163.com>
2017-09-15 10:15:38 +1000

    Modify Listing model, partially complete ViewListingActivity&Layout, add Itemadapter.

--- app/src/main/java/com/riversidecorps/rebuy/ViewListingsActivity.java
+++ app/src/main/java/com/riversidecorps/rebuy/ViewListingsActivity.java
@@ -1,16 +1,188 @@
 package com.riversidecorps.rebuy;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import com.riversidecorps.rebuy.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.riversidecorps.rebuy.adapter.ItemAdapter;
+import com.riversidecorps.rebuy.models.Listing;
 
-public class ViewListingsActivity extends Activity {
+import java.util.ArrayList;
+import java.util.Date;
 
+import static android.content.ContentValues.TAG;
+
+public class ViewListingsActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private RecyclerView mRecyclerView;
+    private ItemAdapter mAdapter;
+    private ArrayList<Listing> mItemList= new ArrayList<>();
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseUser mUser = mAuth.getCurrentUser();
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private static final String AUTH_IN = "onAuthStateChanged:signed_in:";
+    private static final String AUTH_OUT = "onAuthStateChanged:signed_out";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_listings);
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        if (mUser == null) {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        } else {
+            //User is logged in;
+        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, AUTH_IN + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, AUTH_OUT);
+                }
+                // ...
+            }
+        };
+
+
+        // Fetch item listings from Firebase
+
+        // Mock
+        Listing item_one=new Listing("Jimmy", "Valyrian steel", 1, "$1000", "Made in Valyria", new Date());
+        Listing item_two=new Listing("Daenerys", "Dragonglass", 10, "$500", "Made in Dragonstone", new Date());
+        mItemList.add(item_one);
+        mItemList.add(item_two);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mAdapter = new ItemAdapter(this, mItemList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
+
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_my_account) {
+            //Do nothing
+        } else if (id == R.id.nav_message_inbox) {
+            startActivity(new Intent(this, MessageInboxActivity.class));
+        } else if (id == R.id.nav_offers) {
+            startActivity(new Intent(this, OffersActivity.class));
+        } else if (id == R.id.nav_search_listings) {
+            startActivity(new Intent(this, SearchListingsActivity.class));
+        } else if (id == R.id.nav_create_listing) {
+            startActivity(new Intent(this, CreateListingActivity.class));
+        } else if (id == R.id.nav_view_listings) {
+            startActivity(new Intent(this, ViewListingsActivity.class));
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    //On start method
+    @Override
+    public void onStart() {
+        super.onStart();
+        //Sets a listener to catch when the user is signing in.
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    //On stop method
+    @Override
+    public void onStop() {
+        super.onStop();
+        //Sets listener to catch when the user is signing out.
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    /**
+     * Creates the options menu on the action bar.
+     * @param menu Menu at the top right of the screen
+     * @return true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflates the menu menu_other which includes logout and quit functions.
+        getMenuInflater().inflate(R.menu.my_account, menu);
+        return true;
+    }
+
+    /**
+     * Sets a listener that triggers when an option from the taskbar menu is selected.
+     * @param item Which item on the menu was selected.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Finds which item was selected
+        switch(item.getItemId()){
+            //If item is logout
+            case R.id.action_logout:
+                //Sign out of the authenticator and return to login activity.
+                mAuth.signOut();
+                this.startActivity(new Intent(this, LoginActivity.class));
+                return true;
+
+            //If item is reset password
+            case R.id.action_reset_password:
+                this.startActivity(new Intent(this, ResetPasswordActivity.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
