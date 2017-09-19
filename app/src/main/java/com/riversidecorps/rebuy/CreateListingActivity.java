d9b5aacefe5ed27a354a4bffd0ccfeb3dd52d8cb
LL\steven <liuleiwangyue@gmail.com>
2017-09-19 12:50:07 +1000

    connect the view listing and single listing

--- app/src/main/java/com/riversidecorps/rebuy/CreateListingActivity.java
+++ app/src/main/java/com/riversidecorps/rebuy/CreateListingActivity.java
@@ -1,6 +1,5 @@
 package com.riversidecorps.rebuy;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -16,9 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -63,7 +60,6 @@ public class CreateListingActivity extends AppCompatActivity
 
         myFirebaseAuth = FirebaseAuth.getInstance();
         myFirebaseUser = myFirebaseAuth.getCurrentUser();
-        confirmListingBTN.setOnClickListener(this);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
