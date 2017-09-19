1a0aa4494e62daa6bb924bf395d0f3b72a03f62c
LL\steven <liuleiwangyue@gmail.com>
2017-09-19 23:16:37 +1000

    Added sellerid in the class. in order to fetch the data from the class. Compare with the user id and fetch the particular data.

--- app/src/main/java/com/riversidecorps/rebuy/ItemInformation.java
+++ app/src/main/java/com/riversidecorps/rebuy/ItemInformation.java
@@ -10,11 +10,15 @@ public class ItemInformation {
     public int quantity;
     public double price;
     public String description;
+    public String sellerId;
 
-    public ItemInformation(String name, int quantity, double price, String description) {
+
+    public ItemInformation(String name, int quantity, double price, String description,String sellerId) {
         this.name = name;
         this.quantity = quantity;
         this.price = price;
         this.description = description;
+        this.sellerId =sellerId;
+
     }
 }
